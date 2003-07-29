e5a2a792b652bb82cf698033b2b962794a911292
Choe Hwanjin <choe.hwanjin@gmail.com>
2003-07-29 10:22:03 +0900

    gtk_widget_realize를 강제 실행하는 대신 콜백으로 처리함
    git-svn-id: http://kldp.net/svn/nabi/trunk@10 3598d0bf-7bfb-0310-a9b8-e6047f1a0475

--- nabi/src/main.c
+++ nabi/src/main.c
@@ -19,6 +19,15 @@
 NabiApplication* nabi;
 NabiServer* server = NULL;
 
+static void
+on_realize(GtkWidget *widget, gpointer data)
+{
+    g_print("XIM server started...\n");
+    nabi_server_start(server,
+		      GDK_DISPLAY(),
+		      GDK_WINDOW_XWINDOW(widget->window));
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -38,11 +47,9 @@ main(int argc, char *argv[])
     nabi_app_setup_server();
 
     widget = create_main_widget();
-    gtk_widget_show_all(GTK_WIDGET(widget));
-
-    nabi_server_start(server,
-		      GDK_DISPLAY(),
-		      GDK_WINDOW_XWINDOW(widget->window));
+    g_signal_connect_after(G_OBJECT(widget), "realize",
+	    	           G_CALLBACK(on_realize), server);
+    gtk_widget_show(widget);
 
     gtk_main();
 
