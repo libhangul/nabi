1a9798a73692da0c5c1c1f576741dcf7bc505e9c
Choe Hwanjin <choe.hwanjin@gmail.com>
2003-08-23 00:15:33 +0900

    오타 수정
    git-svn-id: http://kldp.net/svn/nabi/trunk/nabi@94 3598d0bf-7bfb-0310-a9b8-e6047f1a0475

--- src/fontset.h
+++ src/fontset.h
@@ -15,4 +15,4 @@ NabiFontSet* nabi_fontset_create   (Display *display, const char *fontset_name);
 void         nabi_fontset_free     (Display *display, XFontSet xfontset);
 void         nabi_fontset_free_all (Display *display);
 
-#endif /* _FONSET_H */
+#endif /* _FONTSET_H */
