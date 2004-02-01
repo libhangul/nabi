63b6b5fdecb498a50ce5d77a53bceb91283328e1
Choe Hwanjin <choe.hwanjin@gmail.com>
2004-02-01 23:57:23 +0900

    프로그램 정보창에서 키 입력 통계를 세로에서 가로 형식으로 수정
    git-svn-id: http://kldp.net/svn/nabi/trunk/nabi@355 3598d0bf-7bfb-0310-a9b8-e6047f1a0475

--- src/ui.c
+++ src/ui.c
@@ -930,82 +930,176 @@ static void get_statistic_string(char *buf, size_t bufsize)
     if (nabi_server == NULL) {
 	snprintf(buf, bufsize, _("XIM Server is not running"));
     } else {
+	char general[256];
+	char choseong[256];
+	char jungseong[256];
+	char jongseong[256];
+
+	snprintf(general, sizeof(general), 
+		 "%s: %d\n"
+		 "\n"
+		 "%s: %3d\n"
+		 "%s: %3d\n"
+		 "%s: %3d\n"
+		 "\n",
+		 _("Connected applications"),
+		 nabi_server->n_connected,
+	     
+		 _("Total"),
+		 nabi_server->statistics.total,
+		 _("BackSpace"),
+		 nabi_server->statistics.backspace,
+		 _("Shift"),
+		 nabi_server->statistics.shift);
+
+	snprintf(choseong, sizeof(choseong),
+                 "%s\n"
+                 "\341\204\200: %-3d "
+                 "\341\204\201: %-3d "
+                 "\341\204\202: %-3d "
+                 "\341\204\203: %-3d "
+                 "\341\204\204: %-3d "
+                 "\341\204\205: %-3d "
+                 "\341\204\206: %-3d "
+                 "\341\204\207: %-3d "
+                 "\341\204\210: %-3d "
+                 "\341\204\211: %-3d "
+                 "\341\204\212: %-3d "
+                 "\341\204\213: %-3d "
+                 "\341\204\214: %-3d "
+                 "\341\204\215: %-3d "
+                 "\341\204\216: %-3d "
+                 "\341\204\217: %-3d "
+                 "\341\204\220: %-3d "
+                 "\341\204\221: %-3d "
+                 "\341\204\222: %-3d ",
+		 _("Choseong"),                     
+                 nabi_server->statistics.jamo[0x00],
+                 nabi_server->statistics.jamo[0x01],
+                 nabi_server->statistics.jamo[0x02],
+                 nabi_server->statistics.jamo[0x03],
+                 nabi_server->statistics.jamo[0x04],
+                 nabi_server->statistics.jamo[0x05],
+                 nabi_server->statistics.jamo[0x06],
+                 nabi_server->statistics.jamo[0x07],
+                 nabi_server->statistics.jamo[0x08],
+                 nabi_server->statistics.jamo[0x09],
+                 nabi_server->statistics.jamo[0x0a],
+                 nabi_server->statistics.jamo[0x0b],
+                 nabi_server->statistics.jamo[0x0c],
+                 nabi_server->statistics.jamo[0x0d],
+                 nabi_server->statistics.jamo[0x0e],
+                 nabi_server->statistics.jamo[0x0f],
+                 nabi_server->statistics.jamo[0x10],
+                 nabi_server->statistics.jamo[0x11],
+                 nabi_server->statistics.jamo[0x12]);
+	
+	snprintf(jungseong, sizeof(jungseong),
+		 "%s\n"
+		 "\341\205\241: %-3d "
+		 "\341\205\242: %-3d "
+		 "\341\205\243: %-3d "
+		 "\341\205\244: %-3d "
+		 "\341\205\245: %-3d "
+		 "\341\205\246: %-3d "
+		 "\341\205\247: %-3d "
+		 "\341\205\250: %-3d "
+		 "\341\205\251: %-3d "
+		 "\341\205\252: %-3d "
+		 "\341\205\253: %-3d "
+		 "\341\205\254: %-3d "
+		 "\341\205\255: %-3d "
+		 "\341\205\256: %-3d "
+		 "\341\205\257: %-3d "
+		 "\341\205\260: %-3d "
+		 "\341\205\261: %-3d "
+		 "\341\205\262: %-3d "
+		 "\341\205\263: %-3d "
+		 "\341\205\264: %-3d "
+		 "\341\205\265: %-3d ",
+		 _("Jungseong"),                    
+                 nabi_server->statistics.jamo[0x61],
+                 nabi_server->statistics.jamo[0x62],
+                 nabi_server->statistics.jamo[0x63],
+                 nabi_server->statistics.jamo[0x64],
+                 nabi_server->statistics.jamo[0x65],
+                 nabi_server->statistics.jamo[0x66],
+                 nabi_server->statistics.jamo[0x67],
+                 nabi_server->statistics.jamo[0x68],
+                 nabi_server->statistics.jamo[0x69],
+                 nabi_server->statistics.jamo[0x6a],
+                 nabi_server->statistics.jamo[0x6b],
+                 nabi_server->statistics.jamo[0x6c],
+                 nabi_server->statistics.jamo[0x6d],
+                 nabi_server->statistics.jamo[0x6e],
+                 nabi_server->statistics.jamo[0x6f],
+                 nabi_server->statistics.jamo[0x70],
+                 nabi_server->statistics.jamo[0x71],
+                 nabi_server->statistics.jamo[0x72],
+                 nabi_server->statistics.jamo[0x73],
+                 nabi_server->statistics.jamo[0x74],
+		 nabi_server->statistics.jamo[0x75]);
+
+	snprintf(jongseong, sizeof(jongseong),
+		 "%s\n"
+                 "\341\206\250: %-3d "
+                 "\341\206\251: %-3d "
+                 "\341\206\252: %-3d "
+                 "\341\206\253: %-3d "
+                 "\341\206\254: %-3d "
+                 "\341\206\255: %-3d "
+                 "\341\206\256: %-3d "
+                 "\341\206\257: %-3d "
+                 "\341\206\260: %-3d "
+                 "\341\206\261: %-3d "
+                 "\341\206\262: %-3d "
+                 "\341\206\263: %-3d "
+                 "\341\206\264: %-3d "
+                 "\341\206\265: %-3d "
+                 "\341\206\266: %-3d "
+                 "\341\206\267: %-3d "
+                 "\341\206\270: %-3d "
+                 "\341\206\271: %-3d "
+                 "\341\206\272: %-3d "
+                 "\341\206\273: %-3d "
+                 "\341\206\274: %-3d "
+                 "\341\206\275: %-3d "
+                 "\341\206\276: %-3d "
+                 "\341\206\277: %-3d "
+                 "\341\207\200: %-3d "
+                 "\341\207\201: %-3d "
+                 "\341\207\202: %-3d ",
+		 _("Jongseong"),
+		 nabi_server->statistics.jamo[0xa8],
+		 nabi_server->statistics.jamo[0xa9],
+		 nabi_server->statistics.jamo[0xaa],
+		 nabi_server->statistics.jamo[0xab],
+		 nabi_server->statistics.jamo[0xac],
+		 nabi_server->statistics.jamo[0xad],
+		 nabi_server->statistics.jamo[0xae],
+		 nabi_server->statistics.jamo[0xaf],
+		 nabi_server->statistics.jamo[0xb0],
+		 nabi_server->statistics.jamo[0xb1],
+		 nabi_server->statistics.jamo[0xb2],
+		 nabi_server->statistics.jamo[0xb3],
+		 nabi_server->statistics.jamo[0xb4],
+		 nabi_server->statistics.jamo[0xb5],
+		 nabi_server->statistics.jamo[0xb6],
+		 nabi_server->statistics.jamo[0xb7],
+		 nabi_server->statistics.jamo[0xb8],
+		 nabi_server->statistics.jamo[0xb9],
+		 nabi_server->statistics.jamo[0xba],
+		 nabi_server->statistics.jamo[0xbb],
+		 nabi_server->statistics.jamo[0xbc],
+                 nabi_server->statistics.jamo[0xbd],
+                 nabi_server->statistics.jamo[0xbe],
+                 nabi_server->statistics.jamo[0xbf],
+                 nabi_server->statistics.jamo[0xc0],
+                 nabi_server->statistics.jamo[0xc1],
+                 nabi_server->statistics.jamo[0xc2]);
+
 	snprintf(buf, bufsize,
-    "%s: %d\n"
-    "\n"
-    "%s: %3d\n"
-    "%s: %3d\n"
-    "%s: %3d\n"
-    "\n"
- 
-     /* choseong               jungseong                 jongseong */
-    "%s           %s           %s\n"
-    "\341\204\200: %-3d        \341\205\241: %-3d        \341\206\250: %-3d\n"
-    "\341\204\201: %-3d        \341\205\242: %-3d        \341\206\251: %-3d\n"
-    "\341\204\202: %-3d        \341\205\243: %-3d        \341\206\252: %-3d\n"
-    "\341\204\203: %-3d        \341\205\244: %-3d        \341\206\253: %-3d\n"
-    "\341\204\204: %-3d        \341\205\245: %-3d        \341\206\254: %-3d\n"
-    "\341\204\205: %-3d        \341\205\246: %-3d        \341\206\255: %-3d\n"
-    "\341\204\206: %-3d        \341\205\247: %-3d        \341\206\256: %-3d\n"
-    "\341\204\207: %-3d        \341\205\250: %-3d        \341\206\257: %-3d\n"
-    "\341\204\210: %-3d        \341\205\251: %-3d        \341\206\260: %-3d\n"
-    "\341\204\211: %-3d        \341\205\252: %-3d        \341\206\261: %-3d\n"
-    "\341\204\212: %-3d        \341\205\253: %-3d        \341\206\262: %-3d\n"
-    "\341\204\213: %-3d        \341\205\254: %-3d        \341\206\263: %-3d\n"
-    "\341\204\214: %-3d        \341\205\255: %-3d        \341\206\264: %-3d\n"
-    "\341\204\215: %-3d        \341\205\256: %-3d        \341\206\265: %-3d\n"
-    "\341\204\216: %-3d        \341\205\257: %-3d        \341\206\266: %-3d\n"
-    "\341\204\217: %-3d        \341\205\260: %-3d        \341\206\267: %-3d\n"
-    "\341\204\220: %-3d        \341\205\261: %-3d        \341\206\270: %-3d\n"
-    "\341\204\221: %-3d        \341\205\262: %-3d        \341\206\271: %-3d\n"
-    "\341\204\222: %-3d        \341\205\263: %-3d        \341\206\272: %-3d\n"
-    "                 \341\205\264: %-3d        \341\206\273: %-3d\n"
-    "                 \341\205\265: %-3d        \341\206\274: %-3d\n"
-    "                                  \341\206\275: %-3d\n"
-    "                                  \341\206\276: %-3d\n"
-    "                                  \341\206\277: %-3d\n"
-    "                                  \341\207\200: %-3d\n"
-    "                                  \341\207\201: %-3d\n"
-    "                                  \341\207\202: %-3d\n",
- 
-    _("Connected applications"),
-    nabi_server->n_connected,
- 
-    _("Total"),
-    nabi_server->statistics.total,
-    _("BackSpace"),
-    nabi_server->statistics.backspace,
-    _("Shift"),
-    nabi_server->statistics.shift,
- 
-    _("Choseong"),                      _("Jungseong"),                     _("Jongseong"),
-    nabi_server->statistics.jamo[0x00], nabi_server->statistics.jamo[0x61], nabi_server->statistics.jamo[0xa8],
-    nabi_server->statistics.jamo[0x01], nabi_server->statistics.jamo[0x62], nabi_server->statistics.jamo[0xa9],
-    nabi_server->statistics.jamo[0x02], nabi_server->statistics.jamo[0x63], nabi_server->statistics.jamo[0xaa],
-    nabi_server->statistics.jamo[0x03], nabi_server->statistics.jamo[0x64], nabi_server->statistics.jamo[0xab],
-    nabi_server->statistics.jamo[0x04], nabi_server->statistics.jamo[0x65], nabi_server->statistics.jamo[0xac],
-    nabi_server->statistics.jamo[0x05], nabi_server->statistics.jamo[0x66], nabi_server->statistics.jamo[0xad],
-    nabi_server->statistics.jamo[0x06], nabi_server->statistics.jamo[0x67], nabi_server->statistics.jamo[0xae],
-    nabi_server->statistics.jamo[0x07], nabi_server->statistics.jamo[0x68], nabi_server->statistics.jamo[0xaf],
-    nabi_server->statistics.jamo[0x08], nabi_server->statistics.jamo[0x69], nabi_server->statistics.jamo[0xb0],
-    nabi_server->statistics.jamo[0x09], nabi_server->statistics.jamo[0x6a], nabi_server->statistics.jamo[0xb1],
-    nabi_server->statistics.jamo[0x0a], nabi_server->statistics.jamo[0x6b], nabi_server->statistics.jamo[0xb2],
-    nabi_server->statistics.jamo[0x0b], nabi_server->statistics.jamo[0x6c], nabi_server->statistics.jamo[0xb3],
-    nabi_server->statistics.jamo[0x0c], nabi_server->statistics.jamo[0x6d], nabi_server->statistics.jamo[0xb4],
-    nabi_server->statistics.jamo[0x0d], nabi_server->statistics.jamo[0x6e], nabi_server->statistics.jamo[0xb5],
-    nabi_server->statistics.jamo[0x0e], nabi_server->statistics.jamo[0x6f], nabi_server->statistics.jamo[0xb6],
-    nabi_server->statistics.jamo[0x0f], nabi_server->statistics.jamo[0x70], nabi_server->statistics.jamo[0xb7],
-    nabi_server->statistics.jamo[0x10], nabi_server->statistics.jamo[0x71], nabi_server->statistics.jamo[0xb8],
-    nabi_server->statistics.jamo[0x11], nabi_server->statistics.jamo[0x72], nabi_server->statistics.jamo[0xb9],
-    nabi_server->statistics.jamo[0x12], nabi_server->statistics.jamo[0x73], nabi_server->statistics.jamo[0xba],
-                                        nabi_server->statistics.jamo[0x74], nabi_server->statistics.jamo[0xbb],
-                                        nabi_server->statistics.jamo[0x75], nabi_server->statistics.jamo[0xbc],
-                                                                            nabi_server->statistics.jamo[0xbd],
-                                                                            nabi_server->statistics.jamo[0xbe],
-                                                                            nabi_server->statistics.jamo[0xbf],
-                                                                            nabi_server->statistics.jamo[0xc0],
-                                                                            nabi_server->statistics.jamo[0xc1],
-                                                                            nabi_server->statistics.jamo[0xc2]);
+		 "%s%s\n\n%s\n\n%s\n", general, choseong, jungseong, jongseong);
     }
 }
 
