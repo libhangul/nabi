2f121bdcaaa267074ddad506adc67f81671d7add
Choe Hwanjin <choe.hwanjin@gmail.com>
2004-11-21 21:53:39 +0900

    각 디렉토리별로 올릴수 있도록 수정함
    git-svn-id: http://kldp.net/svn/nabi/trunk@500 3598d0bf-7bfb-0310-a9b8-e6047f1a0475

--- web/up.sh
+++ web/up.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-scp $* $USER@nabi.kldp.net:/var/lib/gforge/chroot/home/groups/nabi/htdocs
+for i in $@; do
+    dir=`dirname $i`
+    scp $i $USER@nabi.kldp.net:/var/lib/gforge/chroot/home/groups/nabi/htdocs/$dir
+done
